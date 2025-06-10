a6f99200a971d168bd604122a77abe36ac608730
Arthur <thanhdinh744@gmail.com>
2025-06-10 20:27:31 +0700

    Initialize minimal web server

---
+++ README.md
@@ -0,0 +1,17 @@
+# cloudplay
+
+A minimal Go web server used as the starting point for the project.
+
+## Building
+
+```bash
+go build
+```
+
+## Running
+
+```bash
+go run .
+```
+
+The server will listen on `http://localhost:8080` and respond with **Hello, world!**.
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
-package cloudplay
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	})
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
